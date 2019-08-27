424b9132b5b2c62d772ffa4447ec9eee59c78946
Devasish Mudoi <Devasish_Mudoi@Dell.com>
2019-08-27 22:13:35 +0530

    dummy commit

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -4,6 +4,7 @@ DROP DATABASE IF EXISTS tracker_dotnet_test;
 CREATE DATABASE tracker_dotnet_dev;
 CREATE DATABASE tracker_dotnet_test;
 
+
 CREATE USER IF NOT EXISTS 'tracker_dotnet'@'localhost'
   IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON tracker_dotnet_dev.* TO 'tracker_dotnet'@'localhost';
